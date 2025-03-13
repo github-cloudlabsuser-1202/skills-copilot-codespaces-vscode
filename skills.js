961aaddac119eb833d21482e2554c1dfabcdf8a5
github-cloudlabsuser-1202 <167982513+github-cloudlabsuser-1202@users.noreply.github.com>
2025-03-13 21:18:18 +0000

    files

---
+++ skills.js
@@ -0,0 +1,9 @@
+// function to covert Celsius
+// to Fahrenheit
+function convertCelsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Driver code
+let celsius = 60;
+console.log(celsius + "°C is " + convertCelsiusToFahrenheit(celsius) + "°F");
